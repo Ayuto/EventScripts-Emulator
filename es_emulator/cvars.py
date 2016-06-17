555b795e4f18491946de84527e9b338e89c3b067
Ayuto22 <ayuto@web.de>
2016-06-17 20:11:07 +0200

    Added all cvars

--- es_emulator/cvars.py
+++ es_emulator/cvars.py
@@ -2,15 +2,209 @@
 # >> IMPORTS
 # =============================================================================
 # Source.Python
-from cvars import ConVar
+#   Core
 from core import GAME_PATH
+#   Cvars
+from cvars import ConVar
+from cvars.flags import ConVarFlags
 
 # ES Emulator
 from .paths import ES_PATH
 
 
 # =============================================================================
-# >> GLOBAL VARIABLES
+# >> NOT IMPLEMENTED CVARS
+# =============================================================================
+eventscripts_cvar = ConVar(
+    'mattie_eventscripts',
+    '1',
+    'Turns on scripts that run on every server event.',
+    ConVarFlags.REPLICATED | ConVarFlags.NOTIFY
+)
+
+debug_cvar = ConVar(
+    'eventscripts_debug',
+    '0',
+    'Turns on debug print output for EventScripts. Use only if you want to debug problems.'
+)
+
+debugfunctions_cvar = ConVar(
+    'eventscripts_debug_showfunctions',
+    '0',
+    'Adds internal ES function names and line numbers to debug strings for parsing.'
+)
+
+scripttrace_cvar = ConVar(
+    'eventscripts_scripttrace',
+    '0',
+    'Turns on script tracing.'
+)
+
+debuglog_cvar = ConVar(
+    'eventscripts_debuglog',
+    '0',
+    'Will send debug messages to server log as well as console if this is set to 1.'
+)
+
+scriptdir_cvar = ConVar(
+    'eventscripts_subdirectory',
+    '',
+    'Specifies an alternate directory for configuration files. Must be a subdirectory of your cfg directory.'
+)
+
+datadir_cvar = ConVar(
+    'eventscripts_datadirectory',
+    'cfg',
+    'Specifies an alternate directory for output files.'
+)
+
+escape_cvar = ConVar(
+    'eventscripts_escapechars',
+    '; {}()\':',
+    'Lists the characters for that cause variable expansion to require quotes to prevent parsing issues. (Leave this alone unless you know what you\'re doing.)'
+)
+
+cflags_cvar = ConVar(
+    'eventscripts_cflags',
+    '902',
+    'Obsolete. eventscripts_cflags'
+)
+
+timeformat_cvar = ConVar(
+    'eventscripts_timeformat',
+    '%Y-%m-%d %H:%M:%S (%Z)',
+    'Format for time strings retrieved by es_gettimestring.'
+)
+
+currentmap_cvar = ConVar(
+    'eventscripts_currentmap',
+    '',
+    'Current map.'
+)
+
+lastgive_cvar = ConVar(
+    'eventscripts_lastgive',
+    '',
+    'Last given entity index'
+)
+
+deadflag_cvar = ConVar(
+    'eventscripts_deadflag',
+    '1',
+    'If set to 1, uses fast but less stable method of detecting player's dead state. If set to 0, reverts to using Valve's method (delayed).'
+)
+
+cmdprefix_cvar = ConVar(
+    'eventscripts_cmdprefixes',
+    '!',
+    'A list of single character prefixes which will be passed to es_client_command.cfg when fired.'
+)
+
+interface_cvar = ConVar(
+    'eventscripts_eiface',
+    '1',
+    'Internal function for helping EventScripts choose the right ImmediateExecuteCommand for command ordering.'
+)
+
+execmd_cvar = ConVar(
+    'eventscripts_exec-cmd',
+    'exec',
+    'The name of the \'exec\' command that EventScripts uses internally.'
+)
+
+protectrcon_cvar = ConVar(
+    'eventscripts_protectrcon',
+    '1',
+    'Add FCVAR_PROTECTED flag to rcon_password so that it will resolve properly.'
+)
+
+serverdll_cvar = ConVar(
+    'eventscripts_servergamedll_ver',
+    '',
+    'Version of ServerGameDLL used by EventScripts. (read only)'
+)
+
+serverclients_cvar = ConVar(
+    'eventscripts_servergameclients_ver',
+    '',
+    'Version of ServerGameClients interface used by EventScripts. (read only)'
+)
+
+sayevents_cvar = ConVar(
+    'eventscripts_chatevent',
+    '1',
+    'If set to 1, EventScripts will trigger the es_player_chat event, otherwise it will not.'
+)
+
+mapcommands_cvar = ConVar(
+    'eventscripts_maphandler',
+    '1',
+    'If set to 1, EventScripts will allow level change replacement. Must be set in autoexec.cfg or on command-line.'
+)
+
+maxmessages_cvar = ConVar(
+    'eventscripts_maxmsg',
+    '28',
+    'Maximum number of usermessages supported by the mod. Do not change unless instructed to do so.'
+)
+
+quote_cvar = ConVar(
+    'eventscripts_quote',
+    '"',
+    'A quotation mark. A tool, not a variable.'
+)
+
+nextmap_cvar = ConVar(
+    'eventscripts_nextmapoverride',
+    '',
+    'Set this to a map name that will override the next changelevel command to use this map.'
+)
+
+lang_cvar = ConVar(
+    'eventscripts_language',
+    'english',
+    'Name of the default language for EventScripts scripts to use. Supports the same values as cl_language for clients.'
+)
+
+frametimer_cvar = ConVar(
+    'eventscripts_frametimecheck',
+    '0',
+    'If set to 1, EventScripts will output timing warnings when a GameFrame takes longer than 0.01 seconds inside scripts.'
+)
+
+defaultevents_cvar = ConVar(
+    'eventscripts_defaultevents',
+    '1',
+    'If set to 1, EventScripts will try to register for the default event files on each new map.'
+)
+
+autorefreshvars_cvar = ConVar(
+    'eventscripts_autorefreshvars',
+    '1',
+    'If set to 1, EventScripts will try to refresh global change callbacks for every public cvar on each new map.'
+)
+
+shellengine_cvar = ConVar(
+    'eventscripts_shellengine',
+    '0',
+    'Turns on Python-based ESC script engine'
+)
+
+setipcmdline_cvar = ConVar(
+    'eventscripts_setipcmdline',
+    '1',
+    'EventScripts will set the IP for you passed from a +ip command-line.'
+)
+
+python_cvar = ConVar(
+    'mattie_python',
+    '1',
+    'Enable Python support for EventScripts'
+)
+
+
+# =============================================================================
+# >> IMPLEMENTED CVARS
 # =============================================================================
 botcexec_cvar = ConVar(
     'eventscripts_bot-cexec',
