7a84569445862f0e84b31731ca87f16bc5bcbc95
Ayuto22 <ayuto@web.de>
2016-06-19 15:36:56 +0200

    Added eventscripts_version and eventscripts_log

--- es_emulator/cmds.py
+++ es_emulator/cmds.py
@@ -2,7 +2,17 @@
 # >> IMPORTS
 # =============================================================================
 # Source.Python
+#   Commands
 from commands.typed import TypedServerCommand
+from commands.server import ServerCommand
+#   Engines
+from engines.server import engine_server
+
+
+# =============================================================================
+# >> CONSTANTS
+# =============================================================================
+PLUGIN_VERSION_DESCRIPTION = 'Mattie\'s EventScripts, http://www.eventscripts.com, Version:2.1.1.379'
 
 
 # =============================================================================
@@ -22,3 +32,17 @@ def es_unload(command_info, addon):
 def es_reload(command_info, addon):
     import es
     es.reload(addon)
+
+# TODO:
+# Description: prints the version of Mattie's EventScripts plugin
+@ServerCommand('eventscripts_version')
+def on_eventscripts_version(command):
+    import es
+    es.dbgmsg(0, PLUGIN_VERSION_DESCRIPTION)
+
+# TODO:
+# Description: logs the version of Mattie's EventScripts plugin
+@ServerCommand('eventscripts_log')
+def on_eventscripts_log(command):
+    engine_server.log_print(PLUGIN_VERSION_DESCRIPTION)
+    engine_server.log_print('\n')
