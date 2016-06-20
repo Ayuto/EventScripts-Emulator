54edc43165faab67147b6db66130cc00c51acf57
Ayuto22 <ayuto@web.de>
2016-06-20 22:19:34 +0200

    Implemented stack

--- es_emulator/eventscripts/esc/cmds/misc.py
+++ es_emulator/eventscripts/esc/cmds/misc.py
@@ -1,6 +1,7 @@
 from . import Command
 from ..val import sv, VAR
 import es
+from cvars import cvar
 
 @Command(syntax='<tablename> <string>', desc='Update an entry in a stringtable')
 def stringtable(argv):
@@ -58,3 +59,107 @@ def event(argv):
 @Command(syntax='<variable>', desc='Returns 1 in the variable if the server a dedicated server.')
 def isdedicated(argv):
   sv[argv[0]] = es.isdedicated()
+
+stacks = {}
+
+def _create_stack(argv):
+  name = argv[1]
+  if name in stacks:
+    es.dbgmsg(0, 'The stack {} already exists.'.format(name))
+  else:
+    stacks[name] = []
+
+def _delete_stack(argv):
+  name = argv[1]
+  stacks.pop(name, 0)
+
+def _save_stack(argv):
+  var_name = argv[2] if len(argv) > 1 else ''
+  var = cvar.find_var(var_name)
+  if var is not None:
+    name = argv[1]
+    try:
+      stack = stacks[name]
+    except KeyError:
+      es.dbgmsg(0, 'The stack {} couldn\'t be found.'.format(name))
+    else:
+      stack.append(var.get_string())
+  else:
+    es.dbgmsg(0, 'The variable {} does not exist.'.format(var_name))
+
+def _restore_stack(argv):
+  name = argv[1]
+  try:
+    stack = stacks[name]
+  except KeyError:
+    es.dbgmsg(0, 'The stack {} couldn\'t be found.'.format(name))
+  else:
+    if stack:
+      value = stack.pop()
+      var_name = argv[2] if len(argv) > 1 else ''
+      var = cvar.find_var(var_name)
+      if var is not None:
+        var.set_string(value)
+      else:
+        es.dbgmsg(0, 'The var "{}" could not be set'.format(var_name))
+    else:
+      es.dbgmsg(0, 'stack: ERROR: Pop attempt when stack empty: {}'.format(name))
+
+def _pop_stack(argv):
+  name = argv[2] if len(argv) > 1 else ''
+  try:
+    stack = stacks[name]
+  except KeyError:
+    es.dbgmsg(0, 'The stack {} couldn\'t be found.'.format(name))
+  else:
+    if stack:
+      value = stack.pop()
+      var_name = argv[1]
+      var = cvar.find_var(var_name)
+      if var is not None:
+        var.set_string(value)
+      else:
+        es.dbgmsg(0, 'The var "{}" could not be set'.format(var_name))
+    else:
+      es.dbgmsg(0, 'stack: ERROR: Pop attempt when stack empty.')
+
+def _push_stack(argv):
+    name = argv[1]
+    try:
+      stack = stacks[name]
+    except KeyError:
+      es.dbgmsg(0, 'The stack {} couldn\'t be found.'.format(name))
+    else:
+      stack.append(argv[2] if len(argv) > 1 else '')
+
+def _get_stack_length(argv):
+    name = argv[2] if len(argv) > 1 else ''
+    try:
+      stack = stacks[name]
+    except KeyError:
+      es.dbgmsg(0, 'The stack {} couldn\'t be found.'.format(name))
+    else:
+      var_name = argv[1]
+      var = cvar.find_var(var_name)
+      if var is not None:
+        var.set_string(len(stack))
+      else:
+        es.dbgmsg(0, 'The var "{}" could not be set'.format(var_name))
+
+@Command(pre=False, syntax='<operation> <stack id/variable> [stack id/value/variable]', desc='Allows you to do various stack operations with a named stack. Supports create, delete, pop, push, getlength.')
+def stack(argv):
+  operation = argv[0].lower()
+  if operation == 'create':
+    _create_stack(argv)
+  elif operation == 'delete':
+    _delete_stack(argv)
+  elif operation == 'save':
+    _save_stack(argv)
+  elif operation == 'restore':
+    _restore_stack(argv)
+  elif operation == 'pop':
+    _pop_stack(argv)
+  elif operation == 'push':
+    _push_stack(argv)
+  elif operation == 'getlength':
+    _get_stack_length(argv)
