ef45a4880dad1c0e13ccf90b0bd2e6ed86402c05
Ayuto22 <ayuto@web.de>
2016-06-24 19:09:27 +0200

    Added internal ES commands

--- es_emulator/cmds.py
+++ es_emulator/cmds.py
@@ -18,21 +18,6 @@ PLUGIN_VERSION_DESCRIPTION = 'Mattie\'s EventScripts, http://www.eventscripts.co
 # =============================================================================
 # >> ES CONSOLE COMMANDS
 # =============================================================================
-#@TypedServerCommand('es_load')
-def es_load(command_info, addon=None):
-    import es
-    es.load(addon)
-
-#@TypedServerCommand('es_unload')
-def es_unload(command_info, addon):
-    import es
-    es.unload(addon)
-
-#@TypedServerCommand('es_reload')
-def es_reload(command_info, addon):
-    import es
-    es.reload(addon)
-
 # TODO:
 # Description: prints the version of Mattie's EventScripts plugin
 @ServerCommand('eventscripts_version')
@@ -46,3 +31,43 @@ def on_eventscripts_version(command):
 def on_eventscripts_log(command):
     engine_server.log_print(PLUGIN_VERSION_DESCRIPTION)
     engine_server.log_print('\n')
+
+
+# =============================================================================
+# >> UNUSED INTERNAL ES COMMANDS
+# =============================================================================
+unused_internal_commands = (
+    'es__clientcmdproxy',
+    'es__cmdproxy',
+    'es__createentity',
+    'es__createentity2',
+    'es__db',
+    'es__de',
+    'es__disable',
+    'es__dispatcheffect',
+    'es__eb',
+    'es__entblock',
+    'es__entsetname',
+    'es__ep',
+    'es__fire',
+    'es__foreachkey',
+    'es__foreachval',
+    'es__give',
+    'es__ip',
+    'es__prop_dynamic_create',
+    'es__prop_physics_create',
+    'es__pyevent',
+    'es__remove',
+    'es__sb',
+    'es__setang',
+    'es__setpos',
+    'es__unload',
+    'es_x_disable',
+    'es_x_foreachkey',
+    'es_x_foreachval',
+    'es_x_unload',
+    '_mexecl',
+)
+
+for command in unused_internal_commands:
+    ServerCommand(command, 'Internal ES Command')(lambda x: None)
