c504a7b7cf0aa4cca524eff8e809bd4d1b58fa18
Ayuto22 <ayuto@web.de>
2018-01-22 19:22:37 +0100

    Fixed "Command alias already registered"

--- addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/comp.py
+++ addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/comp.py
@@ -4,29 +4,32 @@ from .. import aliases, stack
 from . import Command, Alias
 from ..val import sv
 
-@Command(syntax='[commandname] [[commands]]', pre=False, argsfrom=1, desc='Alias a command.')
-def alias(argv, args):
-  if not argv:
-    es.dbgmsg(0, 'Current alias commands:\n')
-    for alias in aliases:
-      cmdstring = '; '.join('%s %s' % (cmdname, args) for cmdname, argv, args in aliases[alias])
-      es.dbgmsg(0, '%s : %s\n' % (alias, cmdstring))
-  else:
-    name = argv[0]
-    if name in aliases:
-      aliases[name].getcode(args, stack.currentline)
-    elif not es.exists('command', argv[0]):
-      Alias(name).getcode(args, stack.currentline)
-    else:
-      raise RuntimeError('Attempt to alias over an already registered command failed (current ESC limitation)')
-    
+from cvars import cvar
+
+if cvar.find_command('alias') is None:
+    @Command(syntax='[commandname] [[commands]]', pre=False, argsfrom=1, desc='Alias a command.')
+    def alias(argv, args):
+      if not argv:
+        es.dbgmsg(0, 'Current alias commands:\n')
+        for alias in aliases:
+          cmdstring = '; '.join('%s %s' % (cmdname, args) for cmdname, argv, args in aliases[alias])
+          es.dbgmsg(0, '%s : %s\n' % (alias, cmdstring))
+      else:
+        name = argv[0]
+        if name in aliases:
+          aliases[name].getcode(args, stack.currentline)
+        elif not es.exists('command', argv[0]):
+          Alias(name).getcode(args, stack.currentline)
+        else:
+          raise RuntimeError('Attempt to alias over an already registered command failed (current ESC limitation)')
+
 @Command(pre=False, reg=False)
 def wait(desc='Delay code execution for one tick'):
   if sv.eventscripts_currentmap:
     stack.kill()
     sv.save()
     queue(stack.runall)
-      
+
 @Command(pre=False, reg=False, desc='Kills code execution. Very bad. Debug only.')
 def die():
   stack.kill()
