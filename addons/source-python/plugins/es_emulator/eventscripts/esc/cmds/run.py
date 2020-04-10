3f326be4d285292359c088787c0073eab5605a85
Ayuto <ayuto@web.de>
2020-04-10 20:24:17 +0200

    Fixed issue #30

--- addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/run.py
+++ addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/run.py
@@ -9,11 +9,11 @@ import es
 def commandv(argv):
   stack.insertrawline(sv(argv[0]))
   
-@Command(argsfrom=2, syntax='<seconds> <commandstring>', types=FLOAT, desc='Will run <commandstring>, after <seconds> seconds.')
+@Command(argsfrom=3, syntax='<seconds> <commandstring>', types=FLOAT, desc='Will run <commandstring>, after <seconds> seconds.')
 def delayed(argv, args):
   gamethread.delayed(argv[0], stack.queueline, (argv[1], argv[2:], args, esc.stack.currentblock, True))
 
-@Command(syntax='<commandstring>', desc='Adds a command to the end of the command queue.')
+@Command(argsfrom=2, syntax='<commandstring>', desc='Adds a command to the end of the command queue.')
 def soon(argv, args):
   queue(stack.queueline, (argv[0], argv[1:], args, esc.stack.currentblock, True))
 
